af29a1a2fcec5ab456d0efc3dc2d320b6492a3da
Snehal <snehal.webroottech@gmail.com>
2025-09-14 16:26:13 +0000

    code update to git

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    // dd(env('LIVEWIRE_PATH'));
     return view('index');
 });
 
 
 Livewire::setScriptRoute(function ($handle) {
-    return Route::get('/' . env('LIVEWIRE_PATH') . '/livewire/livewire.js', $handle);
+    return Route::get('/' . env('LIVEWIRE_PATH') . '/livewire/livewire.js', $handle)->name('livewireJs');
 });
 
 Livewire::setUpdateRoute(function ($handle) {
-    return Route::get('/' . env('LIVEWIRE_PATH') . '/livewire/update', $handle);
+    return Route::get('/' . env('LIVEWIRE_PATH') . '/livewire/update', $handle)->name('livewireUpdate');
 });
