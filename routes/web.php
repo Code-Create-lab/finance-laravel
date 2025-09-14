5e85fda99a29f65267eceb1e64351c177c78f537
snehal <snehal.webroottech@gmail.com>
2025-09-14 21:49:38 +0530

    livewire path updated

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+
+Livewire::setScriptRoute(function ($handle) {
+    return Route::get('/' . env('LIVEWIRE_PATH') . '/livewire/livewire.js', $handle);
+});
+
+Livewire::setUpdateRoute(function ($handle) {
+    return Route::get('/' . env('LIVEWIRE_PATH') . '/livewire/update', $handle);
+});
