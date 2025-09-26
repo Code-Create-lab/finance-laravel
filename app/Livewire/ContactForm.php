7a0dc47cb12dc79bd8a6eb9ba8c6bd2029807f2a
snehal <snehal.webroottech@gmail.com>
2025-09-26 22:21:38 +0530

    mail

--- app/Livewire/ContactForm.php
+++ app/Livewire/ContactForm.php
@@ -46,7 +46,7 @@ class ContactForm extends Component
         ModelsContactForm::create($formData);
 
 
-        Mail::to(['umeshgrover14@gmail.com', 'pmwaniyojana@gmail.com'])->send(new ContactFormMail($formData));
+        Mail::to(['umeshgrover14@gmail.com', 'pmwaniyojana@gmail.com', 'Umesh@vistara.finance'])->send(new ContactFormMail($formData));
         session()->flash('success', 'Form submitted successfully!');
 
         $this->reset(); // reset form fields
